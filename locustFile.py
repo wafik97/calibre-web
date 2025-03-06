daefb0e12c0b85716ad04a1a4c5f92490c5f960a
wafik97 <wafik.h97@gmail.com>
2025-03-06 10:51:30 +0200

    proj 3

--- locustFile.py
+++ locustFile.py
@@ -1,19 +1,16 @@
 from locust import HttpUser, task, between
 
 class CalibreUser(HttpUser):
-    wait_time = between(1, 3)  # Simulate human-like delays
-    host = "https://ffe4-5-28-174-93.ngrok-free.app"
-
-
+    wait_time = between(1, 3)
+    #host = "http://localhost:8083"
+    host = " https://7fcc-5-28-174-93.ngrok-free.app "
 
     @task
     def view_book_details(self):
-        self.client.get("/book/5")  # Replace with a valid book ID
+        self.client.get("/book/6")
+
 
     @task
     def view_download(self):
         self.client.get("/download/stored/")
 
-    @task
-    def view_download(self):
-        self.client.get("/newest/zyx/")
